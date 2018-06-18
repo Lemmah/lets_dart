c77a4bc34d98d9b4a9a31e7a79326fc700168225
Lemmah <lemayiannakolah@gmail.com>
2018-06-18 13:19:50 +0300

    assign function to a variable

--- functions.dart
+++ functions.dart
@@ -6,6 +6,8 @@ main() {
   // Pass a function a parameter to another function
   var list = [1, 2, 3];
   list.forEach(printElement);
-
+  // Assign function to a variable
+  var loudify = (msg) => '!!! ${msg.toUpperCase()} !!!';
+  print(loudify('hello, lemmah. This is shouting.'));
 
 }
