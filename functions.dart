9e5de8c6772e05827787619c88535573e879142a
Lemmah <lemayiannakolah@gmail.com>
2018-06-18 12:05:02 +0300

    function as parameter to another function

---
+++ functions.dart
@@ -0,0 +1,11 @@
+void printElement(int element) {
+  print(element);
+}
+
+main() {
+  // Pass a function a parameter to another function
+  var list = [1, 2, 3];
+  list.forEach(printElement);
+
+
+}
